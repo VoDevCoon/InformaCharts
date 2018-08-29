3992190d2367d4396f63f033fae39e7add82e915
VoDevCoon <victorzhouau@gmail.com>
2018-08-29 15:08:51 +1000

    deactivate orders for disabled events

--- server/server.js
+++ server/server.js
@@ -1,39 +1,15 @@
 import mongoose from 'mongoose';
-import _ from 'lodash';
 import seed from './util/seed';
 import config from './config/config';
-import Event from './data/eventModel';
-import Order from './data/orderModel';
 import logger from './util/logger';
 
 mongoose.connect(config.db.url, { useNewUrlParser: true });
 
-// setInterval(() => {
-//   seed.newEvents();
-// }, 3600000);
+// seed.syncEvents().then(async (syncEventResult) => {
+//   logger.log(syncEventResult);
+//   seed.syncOrders().then(syncOrderResult => logger.log(syncOrderResult)).catch(err => logger.error(err));
+// }).catch(err => logger.error(err));
 
-// seed.newEvents();
-
-seed.newEvents().then(async (result) => {
-  // Event.find({}).sort({ createdDate: -1 }).exec().then(async (result) => {
-    if (result && result.length > 0) {
-      for (let i = 0; i < result.length; i += 1) {
-        if (result[i].status === 'enable') {
-          await seed.newEventOrders(result[i]);
-        }
-      }
-    }
-  // });
-}).catch((err) => logger.error(err));
-
-
-
-// Event.findOne({ eventId: '32593' }).exec().then((result) => {
-//   const event = result;
-//   Order.aggregate([
-//     { $match: { event: result._id } },
-//     { $group: { _id: '$event', revenue: { $sum: '$total' } } },
-//   ]).exec().then((result) => {
-//     logger.log(_.merge({ eventId: event.eventId, name: event.name }, result));
-//   });
-// });
+seed.syncOrders()
+  .then(syncOrderResult => logger.log(syncOrderResult))
+  .catch(err => logger.error(err));
--- server/util/seed.js
+++ server/util/seed.js
@@ -5,8 +5,6 @@ import Event from '../data/eventModel';
 import Order from '../data/orderModel';
 import config from '../config/config';
 import logger from './logger';
-import { rejects } from 'assert';
-import { resolveObject } from 'url';
 
 const wooCommerceAPI = new WooCommerceAPI({
   url: config.woo.baseUrl,
@@ -16,152 +14,183 @@ const wooCommerceAPI = new WooCommerceAPI({
   version: 'wc/v2',
 });
 
-const onInsert = (err, docs) => {
-  if (err) {
-    logger.error(err.message);
-  } else if (docs.length > 0) {
-    // logger.log(`${docs.length} items were successfully stored!`);
-  } else {
-    logger.log('no new item found.');
-  }
-};
-
 const getDate = yyyymmdd => Date.parse(`${yyyymmdd.substring(0, 4)}-${yyyymmdd.substring(4, 6)}-${yyyymmdd.substring(6, 8)}`);
 
-const searchNewEvents = async (wooCommerce, createdDate, pageIndex, pageSize) => {
-  return new Promise((resolve, reject) => {
+const searchEvents = async (wooCommerce, createdDate, pageIndex, pageSize) => new Promise((resolve, reject) => {
+  const evts = [];
+  const query = `products/?status=publish&after=${createdDate}&per_page=${pageSize}&page=${pageIndex}&orderby=date&order=desc`;
 
-    const evts = [];
-    const query = `products/?status=publish&after=${createdDate}&per_page=${pageSize}&page=${pageIndex}&orderby=date&order=desc`;
+  logger.log(`serching newly added events:\nqueryURL=${query}`);
 
-    // logger.log(`serching newly added events:\nqueryURL=${query}`);
+  wooCommerce.getAsync(query).then((result) => {
+    if (result.statusCode === 200 && result.body.length > 0) {
+      const events = JSON.parse(result.body);
 
-    wooCommerce.getAsync(query).then((result) => {
-      if (result.statusCode === 200 && result.body.length > 0) {
-        const events = JSON.parse(result.body);
+      events.forEach((c) => {
+        const event = {
+          eventId: c.id,
+          name: c.name,
+          eventCode: _.filter(c.meta_data, { key: 'event_code' })[0].value,
+          categories: _.map(c.categories, 'name'),
+          status: _.filter(c.meta_data, { key: 'event_status' })[0].value,
+          startDate: _.filter(c.meta_data, { key: 'starts' })[0].value
+            ? getDate(_.filter(c.meta_data, { key: 'starts' })[0].value)
+            : new Date('2001-01-01'),
+          duration: _.filter(c.meta_data, { key: 'duration' })[0].value,
+          createdDate: Date.parse(`${c.date_created_gmt}Z`), // save date to mongodb in UTC
+        };
 
-        events.forEach((c) => {
-          const event = {
-            eventId: c.id,
-            name: c.name,
-            eventCode: _.filter(c.meta_data, { key: 'event_code' })[0].value,
-            categories: _.map(c.categories, 'name'),
-            status: _.filter(c.meta_data, { key: 'event_status' })[0].value,
-            startDate: _.filter(c.meta_data, { key: 'starts' })[0].value
-              ? getDate(_.filter(c.meta_data, { key: 'starts' })[0].value)
-              : new Date('2001-01-01'),
-            duration: _.filter(c.meta_data, { key: 'duration' })[0].value,
-            createdDate: Date.parse(`${c.date_created_gmt}Z`), // save date to mongodb in UTC
-          };
+        if (_.includes(event.categories, 'Conferences')) {
+          evts.push(event);
+        }
+      });
 
-          if (_.includes(event.categories, 'Conferences')) {
-            evts.push(event);
-          }
+      if (events.length === pageSize) {
+        const nextPage = pageIndex + 1;
+        searchEvents(wooCommerce, createdDate, nextPage, pageSize)
+          .then(results => resolve([...evts, ...results])); // merge the results to evts and resolve to up-level recursive call
+      } else {
+        logger.log('reaching last page for newly added events.');
+        resolve(evts);
+      }
+    } else {
+      logger.log(result);
+      reject(result);
+    }
+  });
+});
+
+const searchOrders = async (wooCommerce, event, createdDate, pageIndex, pageSize) => new Promise((resolve, reject) => {
+  const ods = [];
+  const query = `orders?product=${event.eventId}&per_page=${pageSize}&page=${pageIndex}&after=${createdDate}&orderby=date&order=desc`;
+
+  logger.log(`\n** PAGE ${pageIndex} [Orders] <${event.name}>**\n`);
+  // logger.log(query);
+
+  wooCommerce.getAsync(query).then((result) => {
+    if (result.statusCode === 200 && result.body.length > 0) {
+      const orders = JSON.parse(result.body);
+      orders.forEach((o) => {
+        const order = {
+          orderId: o.id,
+          status: o.status,
+          currency: o.currency,
+          total: o.total,
+          createdDate: Date.parse(`${o.date_created_gmt}Z`), // save date to mongodb in UTC
+          event: event._id,
+        };
+
+        ods.push(order);
+      });
+
+      if (orders.length === pageSize) {
+        const nextPage = pageIndex + 1;
+        searchOrders(wooCommerce, event, createdDate, nextPage, pageSize)
+          .then(results => resolve([...ods, ...results]));
+      } else {
+        logger.log(`reaching last page for <${event.name}>'s new orders.`);
+        resolve(ods);
+      }
+    } else {
+      logger.log(result);
+      reject(result);
+    }
+  });
+});
+
+const searchNewEventOrders = async event => new Promise((resolve, reject) => {
+  Order.findOne({ event: event._id })
+    .sort({ createdDate: -1 })
+    .exec()
+    .then((result) => {
+      let createdDateString = '2018-05-01T00:00:00';
+      if (result) {
+        // search remote using local timestamp as it's saved in locIal time in woocommerce
+        const createdDate = moment(result.createdDate.toISOString());
+        createdDateString = createdDate.tz('Australia/Sydney').format('YYYY-MM-DDTHH:mm:ss');
+      }
+      // search remote woocommerce orders added after the local latest order
+      searchOrders(wooCommerceAPI, event, createdDateString, 1, 100)
+        .then((orders) => {
+          logger.log(`orders of <${event.name}> found: ${orders.length}`);
+          resolve(orders);
+        }).catch(err => reject(err.message));
+    });
+});
+
+const addEventOrders = async event => new Promise((resolve, reject) => {
+  searchNewEventOrders(event)
+    .then((orders) => {
+      const bulkop = Order.collection.initializeUnorderedBulkOp();
+      if (orders && orders.length > 0) {
+        orders.forEach(async (order) => {
+          bulkop.insert(order);
         });
 
-        // try {
-        //   Event.insertMany(evts, onInsert);
-        //   logger.log(`${evts.length} items were successfully stored!`);
-        // } catch (err) {
-        //   reject(err.message);
-        // }
-
-        if (events.length === pageSize) {
-          const nextPage = pageIndex + 1;
-          searchNewEvents(wooCommerce, createdDate, nextPage, pageSize).then((result) => resolve([...evts, ...result])); // merge the results to evts and resolve to up-level recursive call
-        } else {
-          logger.log('reaching last page for newly added events.');
-          resolve(evts);
-        }
+        logger.log(`add orders for <${event.name}>`);
+        bulkop.execute()
+          .then(result => resolve(result))
+          .catch(err => reject(err.message));
       } else {
-        logger.log(result);
+        resolve();
       }
     });
-  });
-};
+});
 
-const searchNewOrders = async (wooCommerce, event, createdDate, pageIndex, pageSize) => {
-  return new Promise((resolve, reject) => {
-    const ods = [];
-    const query = `orders?product=${event.eventId}&per_page=${pageSize}&page=${pageIndex}&after=${createdDate}&orderby=date&order=desc`;
 
-    logger.log(`\n** PAGE ${pageIndex} [Orders] <${event.name}>**\n`);
-    // logger.log(query);
+const deactiveEventOrders = async event => new Promise((resolve, reject) => {
+  logger.log(`deactive orders for <${event.name}>`);
+  const bulkop = Order.collection.initializeUnorderedBulkOp();
 
-    wooCommerce.getAsync(query).then((result) => {
-      if (result.statusCode === 200 && result.body.length > 0) {
-        const orders = JSON.parse(result.body);
-        orders.forEach((o) => {
-          const order = {
-            orderId: o.id,
-            status: o.status,
-            currency: o.currency,
-            total: o.total,
-            createdDate: Date.parse(`${o.date_created_gmt}Z`), // save date to mongodb in UTC
-            event: event._id,
-          };
+  bulkop.find({ event: event._id }).update({ $set: { active: false } });
 
-          ods.push(order);
-        });
+  bulkop.execute()
+    .then(result => resolve(result))
+    .catch(err => reject(err.message));
+});
 
-        // try {
-        //   Order.insertMany(ods, onInsert);
-        //   logger.log(`${ods.length} items were successfully stored!`);
-        // } catch (err) {
-        //   reject(err.message);
-        // }
-
-        if (orders.length === pageSize) {
-          const nextPage = pageIndex + 1;
-          searchNewOrders(wooCommerce, event, createdDate, nextPage, pageSize).then((orders) => resolve([...ods, ...orders]));
-        } else {
-          logger.log(`reaching last page for <${event.name}>'s new orders.`);
-          resolve(ods);
-        }
-      } else {
-        logger.log(result);
-      }
-    });
-  });
-};
+const syncEventOrders = async event => new Promise((resolve, reject) => {
+  if (event.status === 'enable') {
+    addEventOrders(event)
+      .then(result => resolve(result))
+      .catch(err => reject(err.message));
+  } else if (event.status === 'disable') {
+    deactiveEventOrders(event)
+      .then(result => resolve(result))
+      .catch(err => reject(err.message));
+  }
+});
 
 const seed = {
-  newEvents: async () => {
-    return new Promise((resolve) => {
-      Event.findOne({}).sort({ createdDate: -1 }).exec().then((result) => {
-        let createdDateString = '2018-01-10T00:00:00';
-        if (result) {
-          // search remote using local timestamp as it's saved in locIal time in woocommerce
-          const createdDate = moment(result.createdDate.toISOString());
-          createdDateString = createdDate.tz('Australia/Sydney').format('YYYY-MM-DDTHH:mm:ss');
-        }
+  syncEvents: async () => new Promise((resolve, reject) => {
+    const createdDateString = '2018-01-01T00:00:00';
 
-        searchNewEvents(wooCommerceAPI, createdDateString, 1, 100).then((events) => {
-          logger.log(`events found: ${events.length}`);
-          resolve(events)
-        });
-      });
-    });
-  },
+    searchEvents(wooCommerceAPI, createdDateString, 1, 100).then((events) => {
+      logger.log(`events found: ${events.length}`);
 
-  newEventOrders: async (event) => {
-    // get latest event order from local db
-    return new Promise((resolve) => {
-      Order.findOne({ event: event._id }).sort({ createdDate: -1 }).exec().then((result) => {
-        let createdDateString = '2018-01-10T00:00:00';
-        if (result) {
-          // search remote using local timestamp as it's saved in locIal time in woocommerce
-          const createdDate = moment(result.createdDate.toISOString());
-          createdDateString = createdDate.tz('Australia/Sydney').format('YYYY-MM-DDTHH:mm:ss');
-        }
-        // search remote woocommerce orders added after the local latest order
-        searchNewOrders(wooCommerceAPI, event, createdDateString, 1, 100).then((orders) => {
-          logger.log(`orders of <${event.name}> fount: ${orders.length}`);
-          resolve(orders)
-        }).catch((err) => logger.error(err));
+      const bulkop = Event.collection.initializeUnorderedBulkOp();
+      events.forEach((event) => {
+        bulkop.find({ eventId: event.eventId }).upsert().updateOne(event);
       });
+
+      bulkop.execute().then(result => resolve(result)).catch(err => reject(err.message));
     });
-  },
+  }),
+
+  syncOrders: async () => new Promise((resolve) => {
+    Event.find({}).then(async (events) => {
+      const results = [];
+
+      for (let i = 0; i < events.length; i += 1) {
+        const result = await syncEventOrders(events[i]);
+        if (result) {
+          results.push(result);
+        }
+      }
+
+      resolve(results);
+    });
+  }),
 };
 
 module.exports = seed;
